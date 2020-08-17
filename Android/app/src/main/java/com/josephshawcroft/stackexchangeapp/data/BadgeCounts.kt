c88447b94247fdc426a58f7f7c1133cb2963c2ef
Joseph Shawcroft <joeymoto@hotmail.co.uk>
2020-08-17 11:52:33 +0100

    Added user and badge counts data classes

---
+++ Android/app/src/main/java/com/josephshawcroft/stackexchangeapp/data/BadgeCounts.kt
@@ -0,0 +1,7 @@
+package com.josephshawcroft.stackexchangeapp.data
+
+data class BadgeCounts(
+    val bronze: Int,
+    val gold: Int,
+    val silver: Int
+)
--- Android/app/src/main/java/com/josephshawcroft/stackexchangeapp/data/User.kt
+++ Android/app/src/main/java/com/josephshawcroft/stackexchangeapp/data/User.kt
@@ -1,3 +1,21 @@
 package com.josephshawcroft.stackexchangeapp.data
 
-data class User(val id: String)
+data class User(
+    val account_id: Int,
+    val badge_counts: BadgeCounts,
+    val creation_date: Int,
+    val display_name: String,
+    val is_employee: Boolean,
+    val last_access_date: Int,
+    val last_modified_date: Int,
+    val link: String,
+    val profile_image: String,
+    val reputation: Int,
+    val reputation_change_day: Int,
+    val reputation_change_month: Int,
+    val reputation_change_quarter: Int,
+    val reputation_change_week: Int,
+    val reputation_change_year: Int,
+    val user_id: Int,
+    val user_type: String
+)
